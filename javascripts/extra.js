e15d80c5a2fb7664ff2e37134d2ae8c6f94d4332
Rathinavelkumar <Rathinavelkumar.Murugan@gmail.com>
2022-07-23 19:31:43 +0530

    Deployed 4bc6941 with MkDocs version: 1.3.1

---
+++ javascripts/extra.js
@@ -0,0 +1,4 @@
+var script_tag = document.createElement('script');
+script_tag.setAttribute('src','https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6953652220569876');
+script_tag.setAttribute('crossorigin','anonymous');
+document.head.appendChild(script_tag);
